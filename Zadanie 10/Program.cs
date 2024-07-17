366d05d64cc4c637dc435d12e6ecae0536e5ff88
C7PC11 <C7PC11@C7PC11>
2024-07-17 20:40:05 +0200

    Evgeniya zadanie 10

--- Zadanie 10/Program.cs
+++ Zadanie 10/Program.cs
@@ -1,1 +1,8 @@
 ﻿//10. Напишите программу, которая находит среднее арифметическое трех чисел, введенных пользователем, и выводит результат на экран.
+Console.Write("Введите первое число:");
+int x=int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число:");
+int y = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число:");
+int z = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Среднее арифметическое трех чисел: " +(x+y+z)/3);

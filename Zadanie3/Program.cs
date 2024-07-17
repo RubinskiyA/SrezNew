c705d942db12fc48674989081f28983388be3985
a a <doejayne20@gmail.com>
2024-07-17 20:22:14 +0200

    Alexey

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,8 @@
+﻿// апишите программу, которая находит сумму двух чисел, введенных
+//пользователем, и выводит результат на экран.
+int x, y;
+Console.Write("Введите первое число: ");
+int.TryParse(Console.ReadLine(), out x);
+Console.Write("Введите Второе число: ");
+int.TryParse(Console.ReadLine(), out y);
+Console.WriteLine($"{x} + {y} = {x + y}");

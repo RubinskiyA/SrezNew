2a42016868bc513a08b4d97286ed8846d2d08d02
C7PC10 <C7PC10@C7PC10>
2024-07-17 21:21:52 +0200

    Aleksandr

---
+++ Zadanie29/Program.cs
@@ -0,0 +1,15 @@
+﻿Random random = new Random();
+int[][] mas = new int[5][];
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = new int[random.Next(5, 16)];
+}
+for (int i = 0; i < mas.Length; i++)
+{
+    for (int j = 0; j < mas[i].Length; j++)
+    {
+        mas[i][j] = random.Next(10, 100);
+        Console.Write(mas[i][j] + " ");
+    }
+    Console.WriteLine();
+}

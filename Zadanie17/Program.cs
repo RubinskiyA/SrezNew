9aa01817559c8723f9f5493557a6ba56e4462f34
C7PC10 <C7PC10@C7PC10>
2024-07-17 20:58:59 +0200

    Aleksandr

---
+++ Zadanie17/Program.cs
@@ -0,0 +1,17 @@
+﻿//17.Напишите программу, которая проверяет, является ли введенное
+//пользователем слово панграммой (содержит все буквы алфавита).
+string alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+Console.WriteLine("Введите строку для проверки: ");
+string? str=Console.ReadLine();
+int count = 0;
+for (int i=0; i<str.Length; i++)
+{
+    for (int j = 0; j < alphabet.Length; j++)
+    {
+        if (str.ToLower().Contains(alphabet[j])) count++;
+    }
+    break;
+}
+Console.WriteLine(str.ToLower());
+if (count >= 33) Console.WriteLine("Строка панграмма");
+else Console.WriteLine("Строка не панграмма");
---
+++ Zadanie20/Program.cs
@@ -0,0 +1,12 @@
+﻿//20.Создайте одномерный массив длиной 8 элементов, вручную
+//заполните его числами от -10 до 10.
+
+
+Random random = new Random();
+int[] mas = new int[8];
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = random.Next(-10,11);
+}
+foreach (int n in mas) Console.Write (n+" "); 
+

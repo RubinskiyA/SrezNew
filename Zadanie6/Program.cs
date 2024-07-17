02c343ef4d16458f4fb63efefc76434067448eef
a a <doejayne20@gmail.com>
2024-07-17 20:23:49 +0200

    Alexey

---
+++ Zadanie6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая вычисляет факториал введенного
+//пользователем числа и выводит результат на экран;
+
+int n;
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine(), out n);
+int Fact(int n)
+{
+    if (n == 1) return 1;
+    else return n * Fact(n - 1);
+}
+Console.WriteLine($"Факториал числа {n} = {Fact(n)}"); ;

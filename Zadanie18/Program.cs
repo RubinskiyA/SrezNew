2a37e0996e75250184edb213685ebe31280ba296
a a <doejayne20@gmail.com>
2024-07-17 20:35:46 +0200

    Alexey

---
+++ Zadanie18/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая находит наименьшее общее кратное
+//двух чисел, введенных пользователем, и выводит результат на экран.
+int x, y;
+try
+{
+    Console.Write("Введите первое число: ");
+    int.TryParse(Console.ReadLine(), out x);
+    Console.Write("Введите второе число: ");
+    int.TryParse(Console.ReadLine(), out y);
+    if (x == 0 || y == 0) throw new Exception("Введите число, не равное нулю");
+    for (int i = Math.Max(x, y); i < i + 1; i++)
+    {
+        if (i % x == 0 && i % y == 0)
+        {
+            Console.WriteLine(i);
+            break;
+        }
+    }
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+

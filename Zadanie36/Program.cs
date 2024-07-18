f4e4a983e1f7435345a6439ef67dafc29a6d393a
a a <doejayne20@gmail.com>
2024-07-18 16:23:25 +0200

    Alexey

---
+++ Zadanie36/Program.cs
@@ -0,0 +1,33 @@
+﻿// Создать локальную функцию для нахождения наибольшего общего
+//делителя двух чисел.
+
+try
+{
+    int x, y;
+    Console.Write("Введщите первое число: ");
+    int.TryParse(Console.ReadLine(), out x);
+    Console.Write("Введщите второе число: ");
+    int.TryParse(Console.ReadLine(), out y);
+    if (x == 0 || y == 0)
+    {
+        throw new Exception("Введите число не равное нулю");
+    }
+    else Console.WriteLine($"Наибольший общий делитель чисел {x} и {y} - число {MaxDivider(x,y)}");
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+int MaxDivider(int x, int y)
+{
+    int MaxDiv = 1;
+    for (int i=1; i<=Math.Min(x,y); i++)
+    {
+        if(x%i == 0 && y % i == 0)
+        {
+            MaxDiv = i;
+        }
+    }
+    return MaxDiv;
+}

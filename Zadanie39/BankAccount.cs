b6781b6444ba3492c70037187b78c0a5e9b237f5
a a <doejayne20@gmail.com>
2024-07-19 11:03:33 +0200

    Alexey

---
+++ Zadanie39/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie39
+{
+    internal class BankAccount
+    {
+        public decimal AddMoney(decimal balance, decimal addSum)
+        {
+            Console.WriteLine($"Внесено {addSum}");
+            return balance + addSum;
+        }
+        public decimal WithdrawMoney(decimal balance, decimal subSum)
+        {
+            Console.WriteLine($"Снято {subSum}");
+            return balance - subSum;
+        }
+        public void PrintBalance(decimal balance)
+        {
+            Console.WriteLine("Текущий баланс: " + balance);
+        }
+    }
+}

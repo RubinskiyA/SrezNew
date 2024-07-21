768f83ab3784e61756439cb3707f4758c12417c6
SuperPC <SuperPC@SUPERPC>
2024-07-21 09:54:07 +0200

    Aleksandr

---
+++ Zadanie35/Program.cs
@@ -0,0 +1,10 @@
+﻿//35.Написать локальную функцию для вычисления факториала числа.
+long Factorial(int n)
+{
+    if (n == 0) return 1;
+    else return n * Factorial(n - 1);
+}
+Console.Write("Введите n: ");
+int n;
+int.TryParse(Console.ReadLine(), out n);
+Console.WriteLine($"Факториал {n} равен {Factorial(n)}");

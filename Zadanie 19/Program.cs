a07cc39aa85ee615d16c877684dcf0458e883798
ЕвгенияПокидина <ovee9191@gmail.com>
2024-07-21 13:14:02 +0300

    Evgeniya zadanie 19

--- Zadanie 19/Program.cs
+++ Zadanie 19/Program.cs
@@ -5,26 +5,21 @@ class Program
     static void Main()
     {
         int sum = 0;
-
         while (true)
         {
-            Console.Write("Введите число: ");
-            string input = Console.ReadLine()!;
-
-            if (!int.TryParse(input, out int number))
+            Console.Write("Введите число:");
+            string x = Console.ReadLine()!;
+            if (!int.TryParse(x, out int number))
             {
-                Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число.");
+                Console.WriteLine("Некорректный ввод, введите целое число.");
                 continue;
             }
-
             if (number < 0)
             {
                 break;
             }
-
             sum += number;
         }
-
-        Console.WriteLine("Сумма введенных положительных чисел: " + sum);
+        Console.WriteLine("Сумма положительных чисел: " + sum);
     }
 }

d4ca3b567614ea7653b71ed2468705145ba1ddd0
SuperPC <SuperPC@SUPERPC>
2024-07-21 09:31:24 +0200

    Aleksandr

--- Zadanie29/Program.cs
+++ Zadanie29/Program.cs
@@ -1,4 +1,6 @@
-﻿Random random = new Random();
+﻿//29.Найти среднее арифметическое для каждого подмассива.
+
+Random random = new Random();
 int[][] mas = new int[5][];
 for (int i = 0; i < mas.Length; i++)
 {
@@ -6,10 +8,15 @@ for (int i = 0; i < mas.Length; i++)
 }
 for (int i = 0; i < mas.Length; i++)
 {
+    int sum = 0;
     for (int j = 0; j < mas[i].Length; j++)
     {
         mas[i][j] = random.Next(10, 100);
+        sum+= mas[i][j];
         Console.Write(mas[i][j] + " ");
     }
+    Console.WriteLine($"Среднее арифметическое значение {i+1} элемента массива - {sum / mas[i].Length}");
     Console.WriteLine();
 }
+
+

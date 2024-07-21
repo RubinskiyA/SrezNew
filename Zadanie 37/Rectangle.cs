e806bbb17652f52e6cbfa9ffd2defa35ae995112
ЕвгенияПокидина <ovee9191@gmail.com>
2024-07-21 13:04:19 +0300

    Evgeniya zadanie 37

---
+++ Zadanie 37/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_37
+{
+    internal class Rectangle
+    {
+        public double Width { get; set; }
+        public double Lenght { get; set; }
+        public Rectangle(double x, double y)
+        {
+            Width = x;
+            Lenght = y;
+        }
+        public double Perimetr()
+        {
+            return (Width + Lenght) *2;
+        }
+        public double Area()
+        {
+            return Width * Lenght;
+        }
+    }
+}

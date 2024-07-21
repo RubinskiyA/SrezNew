37af846565683b6331e0bba49a1755652b273545
ЕвгенияПокидина <ovee9191@gmail.com>
2024-07-21 13:08:06 +0300

    Evgeniya zadanie 13

--- Zadanie 13/Program.cs
+++ Zadanie 13/Program.cs
@@ -1,9 +1,9 @@
 ﻿// 13. Напишите программу, которая определяет, является ли введенное пользователем число палиндромом (читается одинаково слева направо и справа налево).
 Console.Write("Введите строку:");
-string word=Console.ReadLine()!;
+string str=Console.ReadLine()!;
 string reverse = "";
-for (int i = word.Length - 1; i >= 0; i--) reverse += word[i];
-if (word == reverse) Console.WriteLine("Палиндром");
+for (int i = str.Length - 1; i >= 0; i--) reverse += str[i];
+if (str == reverse) Console.WriteLine("Палиндром");
 else Console.WriteLine("Не палиндром");
 
 
